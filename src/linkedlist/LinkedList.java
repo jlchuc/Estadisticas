e34953a5db977444cb69c56deb26ddc7ade92f16
Jorge L Chuc Lopez <jlchuc@hotmail.com>
2016-09-09 20:14:32 -0500

    Lista doblemente ligada implementada.

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author JorgeLuis
+ */
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+    private int counter;
+
+    public LinkedList() {
+        first = null;
+        last = null;
+        counter = 0;
+    }
+
+    public void add(Node n) {
+        if (first == null) {
+            first = n;
+            last = n;
+            counter = 1;
+        } else {
+            last.setNextNode(n);
+            n.setPreviousNode(last);
+            last = n;
+            counter++;
+        }
+    }
+
+    public int size() {
+        return counter;
+    }
+
+    public Node get(int index) {
+        if (index >= 0 && index < size()) {
+            Node n = first;
+            for (int i = 0; i < index; i++) {
+                n = n.getNextNode();
+            }
+            return n;
+        } else {
+            return null;
+        }
+    }
+
+}
