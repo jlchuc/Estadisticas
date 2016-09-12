c2c92b9bd9e9c4df43e06aa507c98f9cc4a6cf64
Jorge L Chuc Lopez <jlchuc@hotmail.com>
2016-09-12 16:40:38 -0500

    Proyecto Estadísticas. Versión 1.0

--- src/estadisticas/Main.java
+++ src/estadisticas/Main.java
@@ -1,5 +1,11 @@
 package estadisticas;
 
+import dataset.DataSet;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author JorgeLuis
@@ -10,7 +16,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DataSet datos = new DataSet();
+        double valor;
+        try {
+            Scanner entrada = new Scanner(new FileReader("datos.txt"));
+            while (entrada.hasNext()) {
+                valor = entrada.nextDouble();
+                datos.add(valor);
+            }
+        } catch (InputMismatchException ime) {
+            System.err.println("Error en datos de archivo de entrada");
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Archivo de entrada no encontrado");
+        }
+
+        try {
+            System.out.println("Promedio: " + datos.average());
+            System.out.println("Desv. Std.: " + datos.stdDev());
+        } catch (Exception ex) {
+            System.err.println("Error al calcular datos estad�sticos");
+        }
     }
-    
 }
